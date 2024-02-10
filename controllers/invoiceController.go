f56ad2a5a631da26a55ef6681a467b401416d47e
Isha pawar <pawrani159@gmail.com>
2024-02-10 19:45:17 +0530

    made changes

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -16,17 +16,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type InvoiceViewFormat struct {
-	Invoice_id       string
-	Payment_method   string
-	Order_id         string
-	Payment_status   *string
-	Payment_due      interface{}
-	Table_number     interface{}
-	Payment_due_date time.Time
-	Order_details    interface{}
-}
-
 var invoiceCollection *mongo.Collection = database.OpenCollection(database.Client, "invoice")
 
 func GetInvoices() gin.HandlerFunc {
@@ -67,24 +56,8 @@ func GetInvoice() gin.HandlerFunc {
 			return
 		}
 
-		var invoiceView InvoiceViewFormat
 
-		allOrderItems, err := ItemsByOrder(invoice.Order_id)
-		invoiceView.Order_id = invoice.Order_id
-		invoiceView.Payment_due_date = invoice.Payment_due_date
-
-		invoiceView.Payment_method = "null"
-		if invoice.Payment_method != nil {
-			invoiceView.Payment_method = *invoice.Payment_method
-		}
-
-		invoiceView.Invoice_id = invoice.Invoice_id
-		invoiceView.Payment_status = *&invoice.Payment_status
-		invoiceView.Payment_due = allOrderItems[0]["payment_due"]
-		invoiceView.Table_number = allOrderItems[0]["table_number"]
-		invoiceView.Order_details = allOrderItems[0]["order_items"]
-
-		c.JSON(http.StatusOK, invoiceView)
+		c.JSON(http.StatusOK, invoice)
 
 	}
 }
